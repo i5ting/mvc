b7ff2e5419721a006d6157a5ac56bced31ab1fee
狼叔 <langshu.ssl@alibaba-inc.com>
2018-07-03 16:46:06 +0800

    add .gitignore && mount-koa-routes

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const mount = require('mount-koa-routes');
 
 // error handler
 onerror(app)
@@ -33,8 +32,7 @@ app.use(async (ctx, next) => {
 })
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+mount(app,  __dirname + '/routes', true);
 
 // error-handling
 app.on('error', (err, ctx) => {

70d83abfbfd86735589a21172c200e2e517d96de
狼叔 <langshu.ssl@alibaba-inc.com>
2018-07-04 11:47:28 +0800

    refact, please ignore

--- test/models/user.js
+++ test/models/user.js
@@ -1,19 +1,19 @@
 import test from 'ava'
 
 // 1、引入`mongoose connect`
-require('../../db');
+require('../../db')
 
 // 2、引入`User` Model
-const User = require('../../models/user');
+const User = require('../../models/user')
 
 // 3、定义`user` Entity
 let user = new User({
-  username: 'i5ting',
-  password: '0123456789'
-});
+    username: 'i5ting',
+    password: '0123456789'
+})
 
-test('User.save()',async t => {
-  let u = await user.saveAsync()
+test('User.save()', async t => {
+    let u = await user.saveAsync()
 
-  t.is(u.username, 'i5ting');
-});
+    t.is(u.username, 'i5ting')
+})

0a4aca4f9223b2d3729ac4a1e1c571271e9d9d04
狼叔 <langshu.ssl@alibaba-inc.com>
2018-07-04 16:46:06 +0800

    refact, please ignore

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,9 @@
 const router = require('koa-router')()
 const User = require('../models/user')
 
-// router.prefix('/users')
-
 router.get('/', async (ctx, next) => {
   ctx.body = 'this is a users response!'
 })
-
-router.post('/', async (ctx, next) => {
-  ctx.body = 'this is a users response!'
-})
-
 
 router.post('/register', async ctx => {
   if (ctx.request.body.username == undefined || ctx.request.body.password == undefined) {
@@ -19,11 +12,11 @@ router.post('/register', async ctx => {
         code: -2,
         msg: '参数为空'
       }
-    };
+    }
   }
 
-  let name = ctx.request.body.username;
-  let password = ctx.request.body.password;
+  let name = ctx.request.body.username
+  let password = ctx.request.body.password
 
   let u = new User({
     "username": name,
@@ -31,7 +24,7 @@ router.post('/register', async ctx => {
   })
 
   try {
-    let _user = u.saveAsync();
+    let _user = u.saveAsync()
 
     return ctx.body = {
       status: {
@@ -39,7 +32,7 @@ router.post('/register', async ctx => {
         msg: 'sucess'
       },
       data: u
-    };
+    }
   } catch (error) {
     let _user = u.saveAsync()
 
@@ -48,10 +41,8 @@ router.post('/register', async ctx => {
         code: -1,
         msg: 'error' + error.msg
       }
-    };
+    }
   }
-
-});
-
+})
 
 module.exports = router
